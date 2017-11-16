33e74d3c97b165bcfe873f8fb1aaabbd77a61251
Yuttapichai <Yuttapichai@192.168.1.3>
2017-11-16 16:04:19 +0700

    แก้ไขล่าสุด

--- WebContent/js/Level.js
+++ WebContent/js/Level.js
@@ -14,19 +14,22 @@ Level.prototype.create = function() {
 	this.game.physics.arcade.gravity.y = 1000;
 	//this.addMonkey();
 	//this.moveMonkey();
-
 	
 	
+	
+	this.enemies = this.add.group();
 	this.bg = this.game.add.sprite(0, 0, "BG");
-	 this.bg.fixedToCamera = true;
-	 this.bg.width = this.game.width;
-	 this.bg.height = this.game.height;
+	this.bg.fixedToCamera = true;
+	this.bg.width = this.game.width;
+	this.bg.height = this.game.height;
 	 
 	 this.map = this.game.add.tilemap("lab7");
 	 this.map.addTilesetImage('tile_set1');
 	 this.maplayer = this.map.createLayer("Tile Layer 1");
 	 this.maplayer = this.map.createLayer("Tile Layer 2");
 	 
+	 //this.player=this.addPlayer(600,500);
+	// this.player.play("idle");
 	 
 	 //แสดงสไปรท์
 	 for (x in this.map.objects.Object) {
@@ -73,51 +76,7 @@ function gframes(key, n) {
 
 
 
-/*Level.prototype.addMonkey = function() {
-	// add monkey
-	this.monkey = this.add.sprite(this.world.centerX, this.world.height - 250,
-			"player");
-	this.monkey.anchor.set(0.5, 0.5);
 
-	// listen for a monkey click
-	this.monkey.inputEnabled = true;
-	this.monkey.events.onInputDown.add(this.hitMonkey, this);
-};
-
-Level.prototype.moveMonkey = function() {
-	// tween monkey like a yoyo
-	var twn = this.add.tween(this.monkey);
-	twn.to({
-		y : 200
-	}, 100, "Quad.easeInOut", true, 0, Number.MAX_VALUE, true);
-
-	// rotate monkey
-	twn = this.add.tween(this.monkey);
-	twn.to({
-		angle : 360
-	}, 2000, "Linear", true, 0, Number.MAX_VALUE);
-};
-
-Level.prototype.hitMonkey = function() {
-	// stop all monkey's movements
-	this.tweens.removeAll();
-
-	// rotate monkey
-	var twn = this.add.tween(this.monkey);
-	twn.to({
-		angle : this.monkey.angle + 360
-	}, 1000, "Linear", true);
-
-	// scale monkey
-	twn = this.add.tween(this.monkey.scale);
-	twn.to({
-		x : 0.1,
-		y : 0.1
-	}, 1000, "Linear", true);
-
-	// when tween completes, quit the game
-	twn.onComplete.addOnce(this.quitGame, this);
-};*/
 
 Level.prototype.quitGame = function() {
 	this.game.state.start("Menu");
